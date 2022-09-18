855129e211d472eac87bfff842d813ca5996c107
«Dane» <slip_dane@mail.ru>
2022-09-18 23:29:23 +0300

    ЧетНечёт

---
+++ Четное нечетное/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber % 2 == 0 )
+{
+    Console.WriteLine("Число четное!");
+}
+else
+{
+    Console.WriteLine("Число нечетное!");
+}
