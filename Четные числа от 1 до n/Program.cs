c450ca8299a550a33794edd028a8db716f297f67
«Dane» <slip_dane@mail.ru>
2022-09-18 23:34:36 +0300

    ЧетЧисл1-N

---
+++ Четные числа от 1 до n/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int count = 0 ;
+while (count < userNumber - 1) //"-1 костыль,пока не понял как без него"//
+{
+    count += 2;
+    Console.Write(count + " ");
+}
