60ded668661c72701f9f3ed9a65a5495415fea5b
«Dane» <slip_dane@mail.ru>
2022-09-18 23:19:05 +0300

    Add MinMax

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 > userNumber2)
+    {
+        Console.WriteLine("Наибольшим числом является: ");
+        Console.WriteLine(userNumber1);
+        Console.WriteLine("Наименьшим числом является: ");
+        Console.WriteLine(userNumber2);
+    }
+    else if (userNumber1 < userNumber2)
+    {
+        Console.WriteLine("Наибольшим числом является: ");
+        Console.WriteLine(userNumber2);
+        Console.WriteLine("Наименьшим числом является: ");
+        Console.WriteLine(userNumber1);
+}
+
+else if (userNumber1 == userNumber2)
+    {
+        Console.WriteLine("Числа равны! ");
+    }
