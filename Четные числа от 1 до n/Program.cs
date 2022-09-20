b7c76d0f034148b9766b0979c9bf7100705a319c
GorAnatolioDane <113131434+GorAnatolioDane@users.noreply.github.com>
2022-09-20 20:24:03 +0300

    Delete Четные числа от 1 до n directory

--- Четные числа от 1 до n/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Введите число: ");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-int count = 0 ;
-while (count < userNumber - 1) //"-1 костыль,пока не понял как без него"//
-{
-    count += 2;
-    Console.Write(count + " ");
-}
